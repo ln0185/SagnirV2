8ed04d749cbd5d37fc539cb5a3967a3b06130c6b
ln0185 <helene.delaunayratel@gmail.com>
2025-01-30 00:52:17 +0000

    allstories

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import Map from "../../components/Map";
+
 const MapPage = () => {
   return (
     <div className="fixed inset-0">
-      <div className="absolute inset-0 bottom-22"></div>
+      <div className="absolute inset-0 bottom-22">
+        <Map />
+      </div>
     </div>
   );
 };
