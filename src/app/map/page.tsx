2db8388c033fc482085ff29285c1448b167d5906
ln0185 <helene.delaunayratel@gmail.com>
2025-02-06 14:54:30 +0000

    static

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,16 +1,11 @@
 "use client";
-
-import dynamic from "next/dynamic";
-
-const DynamicMap = dynamic(() => import("../../components/Map"), {
-  ssr: false,
-});
+import Map from "../../components/Map";
 
 const MapPage = () => {
   return (
     <div className="fixed inset-0">
       <div className="absolute inset-0 bottom-22">
-        <DynamicMap />
+        <Map />
       </div>
     </div>
   );
